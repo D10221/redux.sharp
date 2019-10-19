e4eb63da46f861c0066c7a5124b3186c5e4ef9e3
Daniel <danieliozzi@gmail.com>
2019-10-19 13:26:40 +0200

    clean ...

--- demo-server/modules/app/src/Loading.tsx
+++
@@ -1,7 +0,0 @@
-import React, { SFC } from "react";
-
-const Loading: (route: string) => SFC<{}> = route => () => {
-  return <div> ... loading {route}</div>;
-};
-
-export default Loading;
--- demo-server/modules/app/src/store/root.ts
+++
@@ -1,2 +0,0 @@
-export const STORE_KEY = "root";
-export const reducer =  (state: any = {}, _action: any) => state;
