e2be85f8298cb5f6fc20134c35cd825300c30f9b
Daniel <daniel@brilliantlink.co.za>
2019-10-24 11:46:11 +0200

    ...

--- server/Program.cs
+++ server/Program.cs
@@ -15,7 +15,6 @@ namespace server
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
