90df77cb3f3841a0e7fe216fa870640b1f8ccfb5
Daniel <danieliozzi@gmail.com>
2019-10-27 10:52:15 +0200

    remove unnecessary usings

--- dapper.fun.test/SelectManyTest.cs
+++ dapper.fun.test/SelectManyTest.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,7 +5,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace dapper.fun.test
 {
     using static dapper.fun.Selects;
-    using static dapper.fun.Queries;
     using static dapper.fun.Connects;
     [TestClass]
     public class SelectManyTest
